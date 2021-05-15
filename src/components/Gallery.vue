10b7e1ff7c942d409183b727eb7bef87e70d3c6f
jgiwinski <juliaiwinski@gmail.com>
2021-05-15 11:57:08 -0700

    Create header component

---
+++ src/components/Gallery.vue
@@ -0,0 +1,32 @@
+<template>
+    <main>
+
+    </main>
+</template>
+
+<script>
+
+export default {
+    name: 'Gallery', 
+    props: {
+        // we will eventually have some
+    }
+}
+</script>
+
+<style scoped>
+
+    header {
+        height: 150px; 
+        background-color: whitesmoke; 
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+    }
+    
+    .prof-img {
+        height: 50px;
+        margin: 20px; 
+    }
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <h1 class='app-title'>Watch to What</h1>
+        <img class='prof-img' src='https://icons-for-free.com/iconfiles/png/512/avatar+human+man+profile+icon-1320085876716628234.png' />
+    </header>
+</template>
+
+<script>
+
+export default {
+    name: 'Header'
+}
+</script>
+
+<style scoped>
+
+    header {
+        height: 150px; 
+        background-color: whitesmoke; 
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+    }
+    
+    .prof-img {
+        height: 50px;
+        margin: 20px; 
+    }
+
+</style>
