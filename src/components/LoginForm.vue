972b40b50a1be2a13cc6cd2d61778aea1f8a7765
jgiwinski <juliaiwinski@gmail.com>
2021-05-16 13:19:37 -0700

    Add conditional rendering to login form for new user form

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,95 @@
+<template>
+  <form v-if="!newUser">
+    <input
+      type="text"
+      name="email"
+      placeholder="EMAIL"/>
+    <label for="email"></label>
+    <input 
+      type="text"
+      name="password"
+      placeholder="PASSWORD"/>
+      <label for="password"></label>
+      <button v-on:click="showNewUser">Don't have an account? Create one here!</button>
+      <button>LOGIN</button>
+  </form>
+  <form v-else>
+    <input
+      type="text"
+      name="email"
+      placeholder="EMAIL"/>
+    <label for="email"></label> 
+    <input 
+      type="text"
+      name="password"
+      placeholder="PASSWORD"/>
+    <label for="password"></label>
+    <input 
+      type="text"
+      name="confirmPassword"
+      placeholder="RETYPE PASSWORD"/>
+    <label for="confirmPassword"></label>
+    <input 
+      type="text"
+      name="firstName"
+      placeholder="FIRST NAME"/>
+    <label for="firstName"></label>
+    <input 
+      type="text"
+      name="lastName"
+      placeholder="LAST NAME"/>
+    <label for="lastName"></label>
+
+    <select v-model="selectedLanguage">
+      <option disabled value="">Default language</option>
+      <option>English</option>
+      <option>Spanish</option>
+      <option>German</option>
+    </select>
+    <select v-model="selectedRegion">
+      <option disabled value="">Default region</option>
+      <option>United States</option>
+      <option>Pits of Hell</option>
+      <option>France</option>
+    </select>
+
+
+      
+    <label for="language"></label>
+    <button>CREATE ACCOUNT</button>
+  </form>
+</template>
+
+
+<script>
+
+export default ({
+  name: 'LoginForm',
+  components: {
+
+  },
+  data() {
+    return {
+      selectedLanguage: '',
+      selectedRegion: '',
+      newUser: false
+    }
+  },
+  methods: {
+    showNewUser() {
+      if (!this.newUser) {
+        this.newUser = true;
+      }
+    }
+  }
+})
+</script>
+
+<style scoped lang='scss'>
+  @import '../index.scss';
+  .hidden {
+    @include hide 
+  }
+
+
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,74 +1,20 @@
 <template>
-  <form>
-    <input
-      type="text"
-      name="email"
-      placeholder="EMAIL"/>
-    <label for="email"></label>
-    <input 
-      type="text"
-      name="password"
-      placeholder="PASSWORD"/>
-      <label for="password"></label>
-      <button>Don't have an account? Create one here!</button>
-      <button>LOGIN</button>
-  </form>
-  <form>
-    <input
-      type="text"
-      name="email"
-      placeholder="EMAIL"/>
-    <label for="email"></label> 
-    <input 
-      type="text"
-      name="password"
-      placeholder="PASSWORD"/>
-    <label for="password"></label>
-    <input 
-      type="text"
-      name="confirmPassword"
-      placeholder="RETYPE PASSWORD"/>
-    <label for="confirmPassword"></label>
-    <input 
-      type="text"
-      name="firstName"
-      placeholder="FIRST NAME"/>
-    <label for="firstName"></label>
-    <input 
-      type="text"
-      name="lastName"
-      placeholder="LAST NAME"/>
-    <label for="lastName"></label>
-    <input 
-      type="text"
-      name="region"
-      placeholder="DEFAULT REGION"/>
-    <label for="region"></label>
-    <select v-model="selected">
-      <option disabled value="">Default language</option>
-      <option>English</option>
-      <option>Spanish</option>
-      <option>German</option>
-    </select>
-
-
-      
-    <label for="language"></label>
-    <button>CREATE ACCOUNT</button>
-  </form>
+    <LoginForm />
 </template>
 
 
 <script>
+import LoginForm from '../components/LoginForm'; 
 
 export default ({
   name: 'Login',
   components: {
-
+    LoginForm
   },
   data() {
     return {
-      selected: ''
+      selectedLanguage: '',
+      selectedRegion: ''
     }
   }
 })
