f3cb4a8b815248faa1b397ba92a94b6cbc430987
jgiwinski <juliaiwinski@gmail.com>
2021-05-16 11:32:53 -0700

    Create login inputs

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,8 +1,20 @@
 <template>
-  <section class="login-view">
-    Login
-  </section>
+  <form>
+      <input
+        type="text"
+        name="email"
+        placeholder="EMAIL"/>
+      <label for="email"></label>
+      <input 
+        type="text"
+        name="password"
+        placeholder="PASSWORD"/>
+        <label for="password"></label>
+        <button>LOGIN</button>
+  </form>
 </template>
+
+
 <script>
 
 export default ({
