56e5c815c8f57c6991cb8c91f2aa4e32f8191cde
Jackson McGuire <jaxmcguire@gmail.com>
2021-05-18 15:57:38 -0400

    Add styling to gallery

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,16 +1,16 @@
 <template>
     <section class='gallery-display'>
-        <h2 class='gallery-title'>
+        <h2 class='gallery-title'>Recommendations</h2>
         <article class='list-container'>
             <li :key="movie.id" v-for="movie in movieList">
-             <MovieCard :movie="movie">
+             <MovieCard :movie="movie"/>
             </li> 
         </article>
     </section>
 </template>
 
 <script>
-import MovieCard from './components/MovieCard'
+import MovieCard from '../components/MovieCard'
 
 export default { 
     name: 'Gallery', 
@@ -22,15 +22,38 @@ export default {
     },
     data() {
         return {
-            movieList: [{id: 0, src: "https://www.indiewire.com/wp-content/uploads/2019/12/us-1.jpg?w=758"}, {id: 1, src:"https://www.indiewire.com/wp-content/uploads/2019/12/us-1.jpg?w=758"}, {id: 2, src: "https://www.indiewire.com/wp-content/uploads/2019/12/us-1.jpg?w=758"}]
+            movieList: [{id: 0, src: "https://www.indiewire.com/wp-content/uploads/2019/12/us-1.jpg?w=758"}, {id: 1, src:"https://www.indiewire.com/wp-content/uploads/2019/12/us-1.jpg?w=758"}, {id: 2, src: "https://www.indiewire.com/wp-content/uploads/2019/12/us-1.jpg?w=758"},  {id: 3, src: "https://www.indiewire.com/wp-content/uploads/2019/12/us-1.jpg?w=758"},  {id: 4, src: "https://www.indiewire.com/wp-content/uploads/2019/12/us-1.jpg?w=758"},  {id: 5, src: "https://www.indiewire.com/wp-content/uploads/2019/12/us-1.jpg?w=758"},  {id: 6, src: "https://www.indiewire.com/wp-content/uploads/2019/12/us-1.jpg?w=758"},  {id: 7, src: "https://www.indiewire.com/wp-content/uploads/2019/12/us-1.jpg?w=758"},  {id: 8, src: "https://www.indiewire.com/wp-content/uploads/2019/12/us-1.jpg?w=758"},  {id: 9, src: "https://www.indiewire.com/wp-content/uploads/2019/12/us-1.jpg?w=758"},  {id: 10, src: "https://www.indiewire.com/wp-content/uploads/2019/12/us-1.jpg?w=758"},  {id: 11, src: "https://www.indiewire.com/wp-content/uploads/2019/12/us-1.jpg?w=758"},  {id: 12, src: "https://www.indiewire.com/wp-content/uploads/2019/12/us-1.jpg?w=758"},  {id: 13, src: "https://www.indiewire.com/wp-content/uploads/2019/12/us-1.jpg?w=758"}]
         }
     }
 }
 </script>
 
 <style scoped lang='scss'>
+@import '../index.scss';
+
     .gallery-display {
-        /* grid display for all films */
+        display: flex;
+        flex-direction: column;
+        text-align: left;
+    }
+
+    .list-container {
+        display: flex;
+        background-color: $gray;
+        overflow-x: scroll;
+        overflow-y: hidden;
+    }
+    .list-container::-webkit-scrollbar {
+        background-color: transparent;
+        border-radius: 10px;
+        height: 55px;
+    }
+
+    .list-container::-webkit-scrollbar-thumb {
+        background-image: url('../assets/image.png');
+        border-radius: 10px;
+        background-size: contain;
+        background-repeat: no-repeat;
     }
 
 </style>
