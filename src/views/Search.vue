2e5005f7883da84c95b99af78cab07ef8a2b8b65
Jackson McGuire <jaxmcguire@gmail.com>
2021-05-19 18:18:11 -0400

    Implement all search components

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,10 +1,26 @@
 <template>
-  <section class="search-view">Search</section>
+  <section class="search-view">
+    <search-form v-on:submitSearch="getSearchResults($event)"></search-form>
+    <search-grid></search-grid>
+  </section>
 </template>
 
 <script>
 
+import SearchForm from '../components/SearchForm'
+import SearchGrid from '../components/SearchGrid'
 export default ({
-  name: 'Search'
+  name: 'Search',
+  components: {
+    SearchForm,
+    SearchGrid
+  },
+  methods: {
+    getSearchResults({lang, region, search, genre}) {
+      const fetchStr = `&language=${lang + '-' + region}&=${search}&watch_region=${region}&with_genres=${genre}`
+      console.log(fetchStr)
+      this.$emit('fetchSearchResults', fetchStr)
+    }
+  }
 })
 </script>
