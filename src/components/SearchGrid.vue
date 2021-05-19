c9fd5c0b4307e9c5ab6455929bc6b776762a8db3
Jackson McGuire <jaxmcguire@gmail.com>
2021-05-19 18:17:44 -0400

    Build implement and style SearchGrid

---
+++ src/components/SearchGrid.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="search-grid">
+      <MovieCard :list="title" v-for="movie in movieList" :movie="movie" :key="movie.id"/>
+  </section>
+</template>
+
+<script>
+import MovieCard from '../components/MovieCard'
+export default {
+  name: 'SearchGrid', 
+  
+  components: {
+      MovieCard,
+  },
+  data() {
+      return {
+          movieList: [{id: 0, title: "Us baby!", src: "https://www.indiewire.com/wp-content/uploads/2019/12/us-1.jpg?w=758"}, {id: 1, title: "Us baby!", src:"https://www.indiewire.com/wp-content/uploads/2019/12/us-1.jpg?w=758"}, {id: 2, title: "Us baby!", src: "https://www.indiewire.com/wp-content/uploads/2019/12/us-1.jpg?w=758"},  {id: 3, title: "Us baby!", src: "https://www.indiewire.com/wp-content/uploads/2019/12/us-1.jpg?w=758"},  {id: 4, title: "Us baby!", src: "https://www.indiewire.com/wp-content/uploads/2019/12/us-1.jpg?w=758"},  {id: 5, title: "Us baby!", src: "https://www.indiewire.com/wp-content/uploads/2019/12/us-1.jpg?w=758"},  {id: 6, title: "Us baby!", src: "https://www.indiewire.com/wp-content/uploads/2019/12/us-1.jpg?w=758"},  {id: 7, title: "Us baby!", src: "https://www.indiewire.com/wp-content/uploads/2019/12/us-1.jpg?w=758"},  {id: 8, title: "Us baby!", src: "https://www.indiewire.com/wp-content/uploads/2019/12/us-1.jpg?w=758"},  {id: 9, title: "Us baby!", src: "https://www.indiewire.com/wp-content/uploads/2019/12/us-1.jpg?w=758"},  {id: 10, title: "Us baby!", src: "https://www.indiewire.com/wp-content/uploads/2019/12/us-1.jpg?w=758"},  {id: 11, title: "Us baby!", src: "https://www.indiewire.com/wp-content/uploads/2019/12/us-1.jpg?w=758"},  {id: 12, title: "Us baby!", src: "https://www.indiewire.com/wp-content/uploads/2019/12/us-1.jpg?w=758"},
+            {id: 13, title: "Us baby!", src: "https://www.indiewire.com/wp-content/uploads/2019/12/us-1.jpg?w=758"}],
+          title: 'Search',
+      
+      }
+  },
+}
+</script>
+
+<style lang="scss">
+@import '../index.scss'; 
+
+  .search-grid {
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    padding: 0 20px;
+    .movie-card {
+      transform: scale(.9);
+      .poster {
+        margin: .75em;
+        box-shadow: 0 0 .75em $lightRed;
+      }
+    }
+  }
+</style>
