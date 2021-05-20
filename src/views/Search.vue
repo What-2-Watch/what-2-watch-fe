106d85cf306ec4f76b5d59181a05a543edde2b7d
Jackson McGuire <jaxmcguire@gmail.com>
2021-05-20 17:10:52 -0400

    Implement fetches to get search results
    Co-authored-by: Julia Iwinski <jgiwinski@users.noreply.github.com>
    Co-authored-by: Matt Umland (he/him) <mattumland@users.noreply.github.com>

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,26 +1,39 @@
 <template>
   <section class="search-view">
-    <search-form v-on:submitSearch="getSearchResults($event)"></search-form>
-    <search-grid></search-grid>
+    <search-form v-on:submitSearch="getSearchResults($event)"
+    ></search-form>
+    <search-grid :searchResults="searchResults"></search-grid>
   </section>
 </template>
 
 <script>
-
 import SearchForm from '../components/SearchForm'
 import SearchGrid from '../components/SearchGrid'
+import { movieSearch } from '../utilities'; 
+
 export default ({
   name: 'Search',
   components: {
     SearchForm,
     SearchGrid
   },
-  methods: {
-    getSearchResults({lang, region, search, genre}) {
-      const fetchStr = `&language=${lang + '-' + region}&=${search}&watch_region=${region}&with_genres=${genre}`
-      console.log(fetchStr)
-      this.$emit('fetchSearchResults', fetchStr)
+  data() {
+    return {
+      searchResults: []
     }
+  },
+  methods: {
+    getSearchResults({ region, search, lang }) {
+      const fetchStr = `&language=${lang}&query=${search}&watch_region=${region}`
+      console.log(fetchStr)
+      movieSearch(fetchStr)
+      .then(data => this.searchResults = data)
+    },
+    // setActiveGenres(genreId) {
+    //   console.log(genreId)
+    // v-on:changeGenreFilter="setActiveGenre($event)"
+    //   // &with_genres=${18}
+    // }
   }
 })
 </script>
