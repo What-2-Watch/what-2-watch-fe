6527032d0ab3e47bbc75656a57c9402b91e83c69
Matt Umland <mattumland@gmail.com>
2021-05-21 09:22:04 -0400

    Set up initial template and script for view

---
+++ src/views/MovieDetailsPage.vue
@@ -0,0 +1,30 @@
+<template>
+    <section>
+        <MovieDetails :movie="movie"/>
+</template>
+
+<script>
+import { MovieDetails } from '../components/MovieDetails'
+import { getMovieDetails } from '../utilities.js'
+
+export default {
+    name: 'MovieDetailsPage',
+    components: {
+        MovieDetails
+    },
+
+    data() {
+        return {
+            movie: {}
+        }
+    },
+    mounted() {
+        Promise.all([getMovieDetails(399566)])
+        .then(response => this.movie = response[0])
+    }
+}
+
+/*
+how to find the id of the movie from within the component, can it be passed 
+*/
+</script>great job
