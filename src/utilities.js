18033b474484151a05ca1add0cda995bb4f66864
Jackson McGuire <jaxmcguire@gmail.com>
2021-05-21 16:02:34 -0400

    Fix postThumb formatting
    Co-authored-by: Matt Umland (he/him) <mattumland@users.noreply.github.com>

--- src/utilities.js
+++ src/utilities.js
@@ -1,9 +1,76 @@
+
 const checkResponse = (response) => {
   if(response.ok) {
   return response.json()
 } else {
   throw new Error('this request isn\'t available')
 }}
+
+export const postWatchlist = (watchListObj) => {
+  console.log(watchListObj)
+  return fetch('https://what-2-watch-be.herokuapp.com/v1/watchlists/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(watchListObj)
+  })
+}
+
+export const postThumb = ({api_movie_id, up}) => {
+  let thumb = {
+      'user': 1,
+      api_movie_id,
+      up,
+      "title": "Sup",
+      "api_actor_id": null,
+      "api_director_id": null,
+      "api_genre_id": null,
+      "api_similar_id": 123,
+  }
+  
+  Promise.all([getCredits(api_movie_id), getMovieGenre(api_movie_id)])
+    .then(responses => {
+      thumb.api_director_id = getDirectorID(responses[0])
+      thumb.api_actor_id = getActorID(responses[0])
+      thumb.api_genre_id = responses[1].id
+  }) 
+
+  return fetch(`https://what-2-watch-be.herokuapp.com/v1/thumbs/`, {
+    method: 'POST',
+    body: JSON.stringify(thumb),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  })
+    .then(response => checkResponse(response))
+    .catch()
+}
+
+const getMovieGenre = (movieID) => {
+  return fetch(`https://api.themoviedb.org/3/movie/${movieID}?api_key=d485a0da5573c3e7d61614d66ae23824&language=en-US`)
+    .then(response => checkResponse(response))
+    .then(data => data.genres[0])
+    .catch(err => console.log(err))
+    
+}
+
+const getCredits = (movieID) => {
+  return fetch (`https://api.themoviedb.org/3/movie/${movieID}/credits?api_key=d485a0da5573c3e7d61614d66ae23824&language=en-US`)
+  .then(response => checkResponse(response))
+  .catch(err => console.log(err))
+}
+
+const getDirectorID = (creditData) => {
+  const director = creditData.crew.find(crew => crew.job === "Director")
+  return director.id
+}
+
+const getActorID = (creditData) => {
+  const actors = creditData.cast.sort((a, b) => a.popularity - b.popularity)
+  console.log(actors)
+  return actors[0].id
+}
 
 export const submitNewUser = (userData) => {
     return fetch(`https://what-2-watch-be.herokuapp.com/v1/users/`, {
@@ -88,16 +155,3 @@ export const cleanRegionData = (array) => {
     }
   })
 }
-
-
-
-
-//get genres
-//get languages
-/*get regions 
-services 
-https://api.themoviedb.org/3/watch/providers/regions?api_key=d485a0da5573c3e7d61614d66ae23824&language=en-US
-*/
-
-
-//
