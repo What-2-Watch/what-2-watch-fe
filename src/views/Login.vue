bd66da26c71272153ab6f674fdcab5dc97563d77
Matt Umland <mattumland@gmail.com>
2021-05-22 17:22:19 -0400

    Update emits to account for service changes

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,11 +3,13 @@
     v-on:createUser="newUser($event)" 
     v-on:existingLogin="userLogin($event)" 
     v-bind:updateLogin="updateLogin" 
+    v-on:postService="addSubscription($event)"
     />
 </template>
 
 
 <script>
+import { postService } from '../utilities.js'
 import LoginForm from '../components/LoginForm'; 
 
 export default ({
@@ -16,7 +18,8 @@ export default ({
     LoginForm
   },
   props: {
-    updateLogin: {type: Function}
+    updateLogin: Function,
+    userId: Number
   },
   emits: [
     'userLogin:user',
@@ -35,8 +38,12 @@ export default ({
     newUser(userData) {
       console.log(userData)
       this.$emit('newUser', userData)
+    },
+    addSubscription(service) {
+      service.user = this.userId
+      console.log(service)
+      postService(service)
     }
-
   }
 
 })
