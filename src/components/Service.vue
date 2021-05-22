8d6513cef2841e42101907538b3bbf615537a653
Matt Umland <mattumland@gmail.com>
2021-05-22 17:20:37 -0400

    Remove text and restyle service button

--- src/components/Service.vue
+++ src/components/Service.vue
@@ -1,8 +1,14 @@
 <template>
-    <section class="service-container">
-        <button class="service-btn">{{ provider.name }}</button>
-        <img :src="provider.logo" :alt="provider.name + 'logo'">
-    </section>
+    <!-- <section class="service-container"> -->
+        <!-- <button class="service-btn">{{ provider.name }}</button> -->
+        <img 
+        v-on:click="addProvider"
+        :src="provider.logo" 
+        class="service-logo" 
+        :alt="provider.name + 'logo'"
+        id="provider.id"
+        name="provider.name">
+    <!-- </section> -->
 </template>
 
 <script>
@@ -11,6 +17,12 @@ export default {
     name: 'Service',
     props: {
         provider: {type: Object}
+    },
+    methods: {
+        addProvider() {
+            console.log(this.provider.name, this.provider.id)
+            this.$emit('addProvider', {name: this.provider.name, api_provider_id: this.provider.id})
+        }
     }
 }
 </script>
@@ -19,11 +31,22 @@ export default {
   @import '../index.scss';
     
     .service-container {
-        @include flex-row; 
-        border: solid 1px $mediumRed; 
-        box-shadow: 0 0 .5em $mediumRed;
-        height: 50px; 
+        // @include flex-row; 
+        // border: solid 1px $mediumRed; 
+        // box-shadow: 0 0 .5em $mediumRed;
+        /* width: 70px;  */
         margin: 5px; 
+
+        img {
+            width: 100%;
+        }
+    }
+
+    .service-logo {
+        width: 75px;
+        height: 75px; 
+        border-radius: 20%;
+
     }
 
     .service-btn {
