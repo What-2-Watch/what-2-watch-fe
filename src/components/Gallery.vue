1c3dac16753ff4a334b842e47005b9186233da69
Jackson McGuire <jaxmcguire@gmail.com>
2021-05-25 19:54:42 -0400

    Remove swiper imports

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -13,14 +13,6 @@
 
 <script>
 import MovieCard from '../components/MovieCard';
-import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
-
-import 'swiper/swiper.scss';
-import 'swiper/components/navigation/navigation.scss';
-import 'swiper/components/pagination/pagination.scss';
-import 'swiper/components/scrollbar/scrollbar.scss';
-
-SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 export default { 
     name: 'Gallery', 
